57c1b638fb8e2c3146526953309e5a41bd48f78b
Jackson <jacksonbull187@gmail.com>
2020-07-08 18:42:36 -0400

    created function that returns the start of the chorus in the 3rd section and number of sections as a dictionary

--- spotify_api/fetch_data.py
+++ spotify_api/fetch_data.py
@@ -27,3 +27,14 @@ def get_audio_features(song_id):
     my_features = {key:value for key, value in feature_dict.items() if key not in ['type', 'id', 'uri', 'track_href','analysis_url']}
     return my_features 
 
+def get_audio_analysis_feat(song_id):
+    #returns the start of the chorus in the 3rd section and number of sections as a dictionary
+    client = spotipy.SpotifyClientCredentials(client_id, client_secret)
+    s_client = spotipy.Spotify(client_credentials_manager=client)
+    analysis = s_client.audio_analysis(song_id)
+    my_analysis_dict = {key:value for key,value in analysis['sections'][2].items() if key == 'start'}
+    my_analysis_dict.update({'sections': len(analysis['sections'][2])})
+    new_key = "chorus_hit"
+    old_key = "start"
+    my_analysis_dict[new_key] = my_analysis_dict.pop(old_key)
+    return my_analysis_dict
