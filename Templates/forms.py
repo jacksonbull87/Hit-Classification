9c1a1c18e55109bf5d27bd65104491e439b89810
Jackson <jacksonbull187@gmail.com>
2020-07-19 16:00:53 -0400

    revised html markup for user inputs

--- Templates/forms.py
+++
@@ -1,8 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-
-class SongSubmitForm(FlaskForm):
-    titlename = StringField('Song Title')
-    artistname = StringField('Artist Name')
-
-    submit = SubmitField('Enter!')
